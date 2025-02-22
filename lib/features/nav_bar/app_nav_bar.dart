f7ad4f54c0c064a009c8eb62d79e4e52647faf53
qasid1999 <qasidqasid809@gmail.com>
2025-02-22 18:05:08 +0500

    UI and apis integrated

---
+++ lib/features/nav_bar/app_nav_bar.dart
@@ -0,0 +1,66 @@
+import 'package:movie_app/core/helpers/imports_helper.dart';
+
+class AppNavBar extends StatelessWidget {
+  const AppNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MovieController>(
+        builder: (context, dashboardProvider, __) {
+      return PopScope(
+        canPop: dashboardProvider.selectBtmIndex != 0 ? false : true,
+        onPopInvokedWithResult: (canPop, __) {
+          if (dashboardProvider.selectBtmIndex != 0) {
+            dashboardProvider.setSelectedBtmIndex(0);
+          }
+        },
+        child: Scaffold(
+          bottomNavigationBar: Container(
+            height: 60.h,
+            decoration: BoxDecoration(
+              color: context.secondaryColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(
+                  25.r,
+                ),
+                topRight: Radius.circular(
+                  25.r,
+                ),
+              ),
+            ),
+            child: BottomNavigationBar(
+              backgroundColor: Colors.transparent,
+              showSelectedLabels: true,
+              currentIndex: dashboardProvider.selectBtmIndex,
+              showUnselectedLabels: true,
+              selectedItemColor: context.onSecondaryColor,
+              selectedLabelStyle:
+                  context.labelSmall!.copyWith(color: context.onSecondaryColor),
+              unselectedLabelStyle:
+                  context.labelSmall?.copyWith(color: context.darkGrey),
+              unselectedItemColor: context.darkGrey,
+              onTap: dashboardProvider.setSelectedBtmIndex,
+              type: BottomNavigationBarType.fixed,
+              items: List.generate(
+                dashboardProvider.icons.length,
+                (index) => BottomNavigationBarItem(
+                  icon: Padding(
+                    padding: EdgeInsets.only(bottom: 5.0.h),
+                    child: SvgPicture.asset(
+                      dashboardProvider.icons[index],
+                      color: dashboardProvider.selectBtmIndex == index
+                          ? context.onSecondaryColor
+                          : context.darkGrey,
+                    ),
+                  ),
+                  label: dashboardProvider.labels[index],
+                ),
+              ),
+            ),
+          ),
+          body: dashboardProvider.bodies[dashboardProvider.selectBtmIndex],
+        ),
+      );
+    });
+  }
+}
---
+++ lib/features/seats/presentation/selected_seat_screen.dart
@@ -0,0 +1,186 @@
+import 'package:movie_app/core/helpers/imports_helper.dart';
+
+class SelectedSeatScreen extends BasePageStatefulScreen {
+  const SelectedSeatScreen({super.key});
+
+  @override
+  BaseStateful<SelectedSeatScreen> createState() => _MovieListScreenState();
+}
+
+class _MovieListScreenState extends BaseStateful<SelectedSeatScreen> {
+  @override
+  Widget buildView(BuildContext context) {
+    return Scaffold(
+      backgroundColor: context.scaffoldBackgroundColor,
+      bottomNavigationBar: Container(
+        color: context.onPrimaryColor,
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Row(
+            children: [
+              Container(
+                width: screenWidth*0.3,
+                decoration: BoxDecoration(
+                  color: context.scaffoldBackgroundColor,
+
+                ),
+              ),
+              SizedBox(
+                width: screenWidth*0.6,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, RoutesPaths.seatsDetailsScreen);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: context.skyBlue,
+                    // Button color
+                    foregroundColor: context.onPrimaryColor,
+                    // Text color
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8), // Rounded corners
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                  ),
+                  child: Text(
+                    "Get Tickets",
+                    style:
+                        context.titleSmall!.copyWith(color: context.onPrimaryColor),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: context.onPrimaryColor,
+        leading: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(
+              Icons.arrow_back_ios_new,
+              size: 20,
+            )),
+        centerTitle: true,
+        title: Column(
+          children: [
+            Text(
+              "The King’s Man",
+              style: context.headlineSmall,
+            ),
+            height5,
+            Text(
+              "In theaters december 22, 2021",
+              style: context.bodySmall!.copyWith(color: context.skyBlue),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          height30,
+          SizedBox(
+            height: 300.h,
+            width: double.infinity,
+            child: InteractiveViewer(
+              boundaryMargin: EdgeInsets.all(18.h),
+              minScale: 0.5,
+              maxScale: 3.0,
+              child: Image.asset(
+                ImagesPaths.imgSeat,
+              ),
+            ),
+          ),
+          height30,
+          Expanded(
+            child: Container(
+              height: 200.h,
+              width: double.infinity,
+              color: context.onPrimaryColor,
+              child: Column(
+                children: [
+                  height30,
+                  Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              IconsPaths.icSeat,
+                              color: context.darkYellow,
+                            ),
+                            width10,
+                            Text(
+                              "Selected",
+                              style: context.labelMedium!
+                                  .copyWith(color: context.darkGrey),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              IconsPaths.icSeat,
+                              color: context.darkGrey,
+                            ),
+                            width10,
+                            Text(
+                              "No available",
+                              style: context.labelMedium!
+                                  .copyWith(color: context.darkGrey),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              IconsPaths.icSeat,
+                              color: context.darkBlue,
+                            ),
+                            width10,
+                            Text(
+                              "VIP(150\$)",
+                              style: context.labelMedium!
+                                  .copyWith(color: context.darkGrey),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              IconsPaths.icSeat,
+                              color: context.skyBlue,
+                            ),
+                            width10,
+                            Text(
+                              "Regular(40\$)",
+                              style: context.labelMedium!
+                                  .copyWith(color: context.darkGrey),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
